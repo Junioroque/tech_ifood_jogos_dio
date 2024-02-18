a0cd5091889c0145cdeb4b15ddeb1b7b8dd1fea8
Junioroque <jcjunioroque@gmail.com>
2024-02-18 17:05:09 -0300

    movimento das cartas

--- projetos/jogo_de_cartas_YuGiOh/src/scripts/engine.js
+++ projetos/jogo_de_cartas_YuGiOh/src/scripts/engine.js
@@ -18,10 +18,86 @@ const state = {
     },
 };
 
+const playerSides = {
+    player1: "player_card",
+    computer: "computer_card"
+};
 
+const pathImages = "./src/assets/icons/";
+
+const cardData = [
+    {
+        id: 0,
+        name: "Blue Eyes White Dragon",
+        type: "Paper",
+        img: `${pathImages}dragon.png`,
+        WinOf: [1],
+        LoseOf: [2],
+    },
+    {
+        id: 1,
+        name: "Dark Magician",
+        type: "Rock",
+        img: `${pathImages}magician.png`,
+        WinOf: [1],
+        LoseOf: [0],
+    },
+    {
+        id: 2,
+        name: "Exodia",
+        type: "Scissors",
+        img: `${pathImages}exodia.png`,
+        WinOf: [0],
+        LoseOf: [1],
+    },
+    
+];
+
+async function getRandomCardId() {
+    const randoIndex = Math.floor(Math.random() * cardData.length);
+    return cardData[randoIndex].id;
+};
+
+async function createCardImage(IdCard, fieldSide) {
+    const cardImage = document.createElement("img");
+    cardImage.setAttribute("height", "100px");
+    cardImage.setAttribute("src", "./src/assets/icons/card-back.png");
+    cardImage.setAttribute("data_id", IdCard);
+    cardImage.classList.add("card");
+
+    if(fieldSide === playerSides.player1) {
+        cardImage.addEventListener("mouseover", () => {
+            drawSelectCard(IdCard);
+        });
+
+        cardImage.addEventListener("click", () => {
+            setCardsField(cardImage.getAttribute("data_id"));
+        });
+
+    };
+    
+    return cardImage;
+};
+
+async function drawSelectCard(index) {
+    state.cardSprites.avatar.src = cardData[index].img;
+    state.cardSprites.name.innerText = cardData[index].name;
+    state.cardSprites.type.innerText = "Attibute : " + cardData[index].type;
+}
+
+async function drawCards(cardNumbes, fieldSide) {
+    for(let i = 0; i < cardNumbes; i++) {
+        const randomIdCard = await getRandomCardId();
+        const cardImage = await createCardImage(randomIdCard, fieldSide);
+    
+        document.getElementById(fieldSide).appendChild(cardImage);
+    };
+    
+};
 
 function init() {
-    
+    drawCards(5, playerSides.player1);
+    drawCards(5, playerSides.computer);
 }
 
 init();
